d2807364b2f42b97685a01b65c87f729848cb16b
JoeShanahan <iam3djoe@gmail.com>
2024-04-13 12:23:44 +0100

    Made a basic book

---
+++ Assets/Scripts/Data/BookData.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LudumDare55
+{
+    [CreateAssetMenu(menuName = "Data/" + nameof(BookData))]
+    public class BookData : ScriptableObject
+    {
+        public string BookName;
+
+        public string Genre;
+
+        [TextArea(3, 10)]
+        public string LongDescription;
+
+        public List<SummonData> Summons;
+
+        public int PageCount
+        {
+            get
+            {
+                int total = 0;
+                
+                foreach (SummonData dat in Summons)
+                {
+                    total += dat.Count;
+                }
+
+                return total;
+            }
+        }
+    }
+
+    [Serializable]
+    public class SummonData
+    {
+        public string Name;
+        public Sprite Sprite;
+        public int Count = 1;
+        public bool DoesStartWith;
+        
+        [TextArea(3, 10)]
+        public string Description;
+    }
+}
