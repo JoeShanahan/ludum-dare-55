0723ac3e08159c383f9628df307b7ceda72cda7d
JoeShanahan <iam3djoe@gmail.com>
2024-04-16 00:15:12 +0100

    aoe visual stuff

--- Assets/Scripts/AoeEffect.cs
+++ Assets/Scripts/AoeEffect.cs
@@ -9,6 +9,7 @@ namespace LudumDare55
 
         [SerializeField] private AnimMode _animMode;
         [SerializeField] private float _lifetime = 0.3f;
+        [SerializeField] private bool _randomRot;
 
         private void OnEnable()
         {
@@ -25,6 +26,11 @@ namespace LudumDare55
                 transform.localScale = new Vector3(1, 0, 1);
                 transform.DOScaleY(1, _lifetime * 0.3f).SetEase(Ease.OutExpo);
                 transform.DOScaleY(0, _lifetime * 0.7f).SetEase(Ease.InSine).SetDelay(_lifetime * 0.3f);
+            }
+
+            if (_randomRot)
+            {
+                transform.eulerAngles = new Vector3(0, 0, Random.Range(0, 360));
             }
         }
     }
